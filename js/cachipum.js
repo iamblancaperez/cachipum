4bfeeaa323f54731c51e31f86be58063eebc30b5
Blanca Perez <blankp@gmail.com>
2016-10-04 13:13:26 -0300

    Added input validation of user / Added infinite loop for continue playing

--- js/cachipum.js
+++ js/cachipum.js
@@ -4,55 +4,71 @@ va a ser piedra papel o tijera
 */
 var jugadaUser;
 var jugadaCompu;
-
-var jugadaUser = prompt("¿Piedra, papel o tijera?");
-console.log(jugadaUser);
-
-/*Busco un numero random para la seleccion
-de la opcion de la computadora*/
-var numRandom = Math.random()*2;
-var numRedondo = Math.round(numRandom);
-console.log(numRedondo);
-
-/*0: piedra / 1: tijera / 2: papel*/
-
-if(numRedondo == 0){
-	jugadaCompu = "piedra";
-}else if(numRedondo == 1){
-	jugadaCompu = "tijera";
-}else{
-	jugadaCompu = "papel";
-}
-
-alert("Ahora le toca el computador elegir");
-console.log(jugadaCompu);
-alert("El computador eligió : " + jugadaCompu);
-if(jugadaUser == "piedra"){
-	if(jugadaCompu == "piedra"){
-		alert("Oh! hubo un empate");
-	}else if(jugadaCompu == "tijera"){
-		alert("Felicitaciones has ganado!!!");
-	}else{
-		alert("Oh no!!! La computadora ha ganado");
+var numRandom;
+var numRedondo;
+var confirm;
+//La condicion para detenerse es que el input
+//sea piedra papel o tijera
+do{
+	while(!(jugadaUser === "piedra" || 
+		    jugadaUser === "papel"  || 
+		    jugadaUser === "tijera"))
+	{
+		//Aca pregunto al usuario
+		jugadaUser = prompt("¿piedra, papel o tijera?").toLowerCase();
+		console.log(jugadaUser);
 	}
-}else if(jugadaUser == "tijera"){
-	if(jugadaCompu == "tijera"){
-		alert("Es un empate");
-	}else if(jugadaCompu == "papel"){
-		alert("Felicitaciones has ganado");
-	}else{
-		alert("Ganó la compu");
-	}
-}else{//Si no es piedra ni tijera es papel
-	if(jugadaCompu == "papel"){
-		alert("Empate");
-	}else if(jugadaCompu == "piedra"){
-		alert("Ganaste!!!!")
-	}else{
-		alert("Ganó el compu!!!")
-	}
-} 
+	/*Busco un numero random para la seleccion
+	de la opcion de la computadora*/
+	numRandom = Math.random()*2;
+	numRedondo = Math.round(numRandom);
+	console.log(numRedondo);
 
+	/*0: piedra / 1: tijera / 2: papel*/
+
+	if(numRedondo == 0){
+		jugadaCompu = "piedra";
+	}else if(numRedondo == 1){
+		jugadaCompu = "tijera";
+	}else{
+		jugadaCompu = "papel";
+	}
+
+	alert("Ahora le toca el computador elegir");
+	console.log(jugadaCompu);
+	alert("El computador eligió : " + jugadaCompu);
+
+	if(jugadaUser == "piedra"){
+		if(jugadaCompu == "piedra"){
+			alert("Oh! hubo un empate");
+		}else if(jugadaCompu == "tijera"){
+			alert("Felicitaciones has ganado!!!");
+		}else{
+			alert("Oh no!!! La computadora ha ganado");
+		}
+	}else if(jugadaUser == "tijera"){
+		if(jugadaCompu == "tijera"){
+			alert("Es un empate");
+		}else if(jugadaCompu == "papel"){
+			alert("Felicitaciones has ganado");
+		}else{
+			alert("Ganó la compu");
+		}
+	}else if(jugadaUser == "papel"){//Si no es piedra ni tijera es papel
+		if(jugadaCompu == "papel"){
+			alert("Empate");
+		}else if(jugadaCompu == "piedra"){
+			alert("Ganaste!!!!")
+		}else{
+			alert("Ganó el compu!!!")
+		}
+	}else{
+		alert("Su opción no es válida");
+	}
+	jugadaUser = "";
+	confirmation = confirm("¿Desea jugar de nuevo?");
+	console.log(confirmation);
+}while(confirmation)
 
 
 
